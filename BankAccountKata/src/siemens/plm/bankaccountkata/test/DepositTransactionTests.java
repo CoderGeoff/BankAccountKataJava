31052a84ac04512df3896730191509b4dc4a5cce
Geoff Bones <geoff.bones@siemens.com>
2015-08-27 18:44:35 +0100

    Whitespace changes

--- BankAccountKata/src/siemens/plm/bankaccountkata/test/DepositTransactionTests.java
+++ BankAccountKata/src/siemens/plm/bankaccountkata/test/DepositTransactionTests.java
@@ -17,19 +17,18 @@ public class DepositTransactionTests {
 
 	@Test
 	public void givenAnAccountWith$1AndNoRules_WhenIDeposit$1_ThenBalanceShouldBe$2() {
-		
-    // Given
-	BankAccount account = new BankAccount(AccountType.CheckingAccount);
-	account.setBalance(100);
-		
-	// When
-	DepositTransaction depositIntoAccount = new DepositTransaction(account);
-	depositIntoAccount .Execute(100);
-
-	// Then
-	assertEquals(200, account.getBalance());
+			
+	    // Given
+		BankAccount account = new BankAccount(AccountType.CheckingAccount);
+		account.setBalance(100);
+			
+		// When
+		DepositTransaction depositIntoAccount = new DepositTransaction(account);
+		depositIntoAccount .Execute(100);
+	
+		// Then
+		assertEquals(200, account.getBalance());
 	}
-
 
 	@Test
 	public void givenCollaborator1ReturnsLessThan0_WhenICallTheMethodIWantToTest_ThenCollaborator2ShouldReceiveValue() {
@@ -50,22 +49,21 @@ public class DepositTransactionTests {
 	    // Then
 	    verify(coll2, times(1)).Method2(expectedValue);
 	}
-
 	
 	@Test
     public void givenCollaborator1ReturnsLessThan0_WhenICallTheMethodIWantToTest_ThenTheResultShouldBeMinus1() {
-    ICollaborator1 collaborator1 = mock(ICollaborator1.class);
-    ICollaborator2 collaborator2 = mock(ICollaborator2.class);
-    ICollaborator3 collaborator3 = mock(ICollaborator3.class);
+		ICollaborator1 collaborator1 = mock(ICollaborator1.class);
+		ICollaborator2 collaborator2 = mock(ICollaborator2.class);
+		ICollaborator3 collaborator3 = mock(ICollaborator3.class);
     
-    // Given
-    MyClass myObject = new MyClass(collaborator1, collaborator2, collaborator3);
-    when(collaborator1.Method1()).thenReturn(-2);
+		// Given
+		MyClass myObject = new MyClass(collaborator1, collaborator2, collaborator3);
+		when(collaborator1.Method1()).thenReturn(-2);
 
-    // When
-    int result = myObject.BehaviorIWantToTest();
-
-    // Then
-    assertEquals(-1, result);
-}
+	    // When
+	    int result = myObject.BehaviorIWantToTest();
+	
+	    // Then
+	    assertEquals(-1, result);
+	}
 }
